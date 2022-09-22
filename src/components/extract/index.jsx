eff99c4fa936fd3b3f7b536a2090b22f7be53dd8
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-09-22 15:27:41 -0300

    fix - order

--- src/components/extract/index.jsx
+++ src/components/extract/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Order from "../order";
 import {StyledProductDetails, StyledHeader, StyledContainer, StyledProduct, StyledBar } from "./style";
 
 const Extract = () => {
@@ -13,12 +12,11 @@ const Extract = () => {
       <StyledBar />
 
       <StyledProduct>
-        {Order.map}
         <StyledProductDetails>
-          <h2>{localStorage.getItem("transaction")}</h2>
-          <h2>{localStorage.getItem("product")}</h2>
+          <h2>+</h2>
+          <h2>Lorem ipsum </h2>
         </StyledProductDetails>
-        <h2>{localStorage.getItem("price")}</h2>
+        <h2>R$99,99</h2>
       </StyledProduct>
 
       <StyledBar />
--- src/components/order/index.jsx
+++ src/components/order/index.jsx
@@ -1,4 +1,4 @@
-import { React, useState} from "react";
+import { React, useEffect, useState } from "react";
 import {
   StyledBar,
   Styledbutton,
@@ -7,26 +7,30 @@ import {
   StyledSelect,
 } from "./style";
 
-
 const Order = () => {
-const [transaction, setTransaction] = useState();
-const [product, setProduct] = useState();
-const [price, setPrice] = useState();
-const [order, setOrder] = useState([]);
+  const [transaction, setTransaction] = useState();
+  const [product, setProduct] = useState();
+  const [price, setPrice] = useState();
 
-const handleOrder = () => {
-  localStorage.setItem("transaction",transaction)
-  localStorage.setItem("product",product)
-  localStorage.setItem("price",price)
-}
+  const handleOrder = () => {
+    var orders = JSON.parse(localStorage.getItem("orders") || []);
+    var order = {
+      transaction: transaction,
+      product: product,
+      price: price,
+    };
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+  };
 
   return (
     <StyledOrderContainer onSubmit={handleOrder}>
       <label htmlFor="type">Tipo de Transação</label>
       <StyledSelect
-      id="transaction"
-      name="transaction"
-      onChange={(e) => setTransaction(e.target.value)}>
+        id="transaction"
+        name="transaction"
+        onChange={(e) => setTransaction(e.target.value)}
+      >
         <option value="default">Selecione o tipo</option>
         <option value="Compra">Compra</option>
         <option value="Venda">Venda</option>
@@ -43,13 +47,13 @@ const handleOrder = () => {
 
       <label htmlFor="text">Valor</label>
       <StyledInput
-      id="productPrice"
-      name="productPrice"
-      type="input"
-       placeholder="R$ 0,00"
-       onChange={(e) => setPrice(e.target.value)}
-        />
-      <Styledbutton>  Adicionar Transação </Styledbutton>
+        id="productPrice"
+        name="productPrice"
+        type="input"
+        placeholder="R$ 0,00"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <Styledbutton> Adicionar Transação </Styledbutton>
 
       <StyledBar />
     </StyledOrderContainer>
